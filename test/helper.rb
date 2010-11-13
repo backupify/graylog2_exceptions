a868aef39ce41a733cb1901fed077051faa9f425
Lennart Koopmann <lennart@socketfeed.com>
2010-11-13 02:08:13 +0100

    better use this

--- test/helper.rb
+++ test/helper.rb
@@ -3,4 +3,4 @@ require 'zlib'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'graylog2_exceptions'
+require 'graylog2_exceptions.rb'
